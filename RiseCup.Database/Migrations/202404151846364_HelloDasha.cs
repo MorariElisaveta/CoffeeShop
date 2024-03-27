f546c765002cce51c6bc59ee7d9725a5cc0a09ac
Dashkadobr <Dashkadobr@example.com>
2024-03-27 22:59:57 +0300

    add migrations

---
+++ RiseCup.Database/Migrations/202404151846364_HelloDasha.cs
@@ -0,0 +1,95 @@
+﻿namespace RiseCup.Database.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class HelloDasha : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        CategoryId = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(),
+                        Description = c.String(),
+                    })
+                .PrimaryKey(t => t.CategoryId);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        ProductId = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Description = c.String(),
+                        CategoryId = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.ProductId)
+                .ForeignKey("dbo.Categories", t => t.CategoryId)
+                .Index(t => t.CategoryId);
+            
+            CreateTable(
+                "dbo.OrderItems",
+                c => new
+                    {
+                        OrderItemId = c.String(nullable: false, maxLength: 128),
+                        Quantity = c.Int(nullable: false),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        ProductId = c.String(maxLength: 128),
+                        OrderId = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.OrderItemId)
+                .ForeignKey("dbo.Orders", t => t.OrderId)
+                .ForeignKey("dbo.Products", t => t.ProductId)
+                .Index(t => t.ProductId)
+                .Index(t => t.OrderId);
+            
+            CreateTable(
+                "dbo.Orders",
+                c => new
+                    {
+                        OrderId = c.String(nullable: false, maxLength: 128),
+                        OrderDate = c.DateTime(nullable: false),
+                        UserId = c.String(maxLength: 128),
+                        TotalAmount = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Status = c.String(),
+                    })
+                .PrimaryKey(t => t.OrderId)
+                .ForeignKey("dbo.Users", t => t.UserId)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        Username = c.String(),
+                        PasswordHash = c.Binary(),
+                        PasswordSalt = c.Binary(),
+                        Role = c.String(),
+                    })
+                .PrimaryKey(t => t.UserId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.OrderItems", "ProductId", "dbo.Products");
+            DropForeignKey("dbo.Orders", "UserId", "dbo.Users");
+            DropForeignKey("dbo.OrderItems", "OrderId", "dbo.Orders");
+            DropForeignKey("dbo.Products", "CategoryId", "dbo.Categories");
+            DropIndex("dbo.Orders", new[] { "UserId" });
+            DropIndex("dbo.OrderItems", new[] { "OrderId" });
+            DropIndex("dbo.OrderItems", new[] { "ProductId" });
+            DropIndex("dbo.Products", new[] { "CategoryId" });
+            DropTable("dbo.Users");
+            DropTable("dbo.Orders");
+            DropTable("dbo.OrderItems");
+            DropTable("dbo.Products");
+            DropTable("dbo.Categories");
+        }
+    }
+}
