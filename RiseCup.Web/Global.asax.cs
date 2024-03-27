a549a942bce8ae6fa90db4520f7692dd7cb2aacb
MorariElisaveta <MorariElisaveta@example.com>
2024-03-27 21:18:19 +0300

    Add post auth method

--- RiseCup.Web/Global.asax.cs
+++ RiseCup.Web/Global.asax.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Principal;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Security;
 
 namespace RiseCup.Web
 {
@@ -17,5 +21,35 @@ namespace RiseCup.Web
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+        
+        protected void Application_PostAuthenticateRequest(object sender, EventArgs e)
+        {
+            var authCookie = HttpContext.Current.Request.Cookies[FormsAuthentication.FormsCookieName];
+            if (authCookie != null && !string.IsNullOrEmpty(authCookie.Value))
+            {
+                try
+                {
+                    var authTicket = FormsAuthentication.Decrypt(authCookie.Value);
+                    if (authTicket != null && !authTicket.Expired)
+                    {
+                        // Разделение ролей пользователя, указанных в билете
+                        var userRoles = authTicket.UserData.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                        // Создание идентичности и принципала с учетом ролей
+                        var userIdentity = new FormsIdentity(authTicket);
+                        var userPrincipal = new GenericPrincipal(userIdentity, userRoles);
+
+                        // Присваивание контексту HTTP созданного принципала
+                        HttpContext.Current.User = userPrincipal;
+                    }
+                }
+                catch (CryptographicException ex)
+                {
+                    // Логирование ошибки расшифровки куки
+                    Trace.Write("Error decrypting cookie: " + ex.Message);
+                    FormsAuthentication.SignOut(); // Очистка невалидной аутентификационной куки
+                }
+            }
+        }
+
     }
 }
