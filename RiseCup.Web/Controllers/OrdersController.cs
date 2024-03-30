785e95735f7c89fb69732c58ad4f16c94e538e94
MorariElisaveta <MorariElisaveta@example.com>
2024-03-30 13:17:54 +0300

    add controller only for admin

---
+++ RiseCup.Web/Controllers/OrdersController.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using RiseCup.Database;
+
+namespace RiseCup.Web.Controllers
+{
+    [Authorize(Roles = "Administrator")] // Доступ только для администраторов
+    public class OrdersController : Controller
+    {
+        private readonly RiseCupContext _db;
+
+        public OrdersController()
+        {
+            _db = new RiseCupContext(); // Инициализация контекста базы данных
+        }
+
+        public ActionResult Index()
+        {
+            var orders = _db.Orders.ToList(); // Получение всех заказов из базы данных
+            return View(orders); // Передача списка заказов в представление
+        }
+    }
+}
