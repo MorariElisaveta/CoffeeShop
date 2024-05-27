0bd774e78f381e73cb15f9de484de7ce3d541184
MorariElisaveta <MorariElisaveta@example.com>
2024-05-27 07:30:29 +0300

    Last changes

---
+++ RiseCup.Web/Attributes/AuthorizeAttribute.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RiseCup.Web.Attributes
+{
+    public class SimpleAuthorizeAttribute : ActionFilterAttribute
+    {
+        private readonly string[] _requiredRoles;
+
+        public SimpleAuthorizeAttribute(params string[] requiredRoles)
+        {
+            _requiredRoles = requiredRoles;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var httpContext = filterContext.HttpContext;
+
+            if (!httpContext.User.Identity.IsAuthenticated)
+            {
+                HandleUnauthorizedRequest(filterContext);
+                return;
+            }
+
+            if (!_requiredRoles.Any(httpContext.User.IsInRole))
+            {
+                HandleUnauthorizedRequest(filterContext);
+                return;
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+
+        private void HandleUnauthorizedRequest(ActionExecutingContext filterContext)
+        {
+            if (filterContext.HttpContext.User.Identity.IsAuthenticated)
+            {
+                filterContext.Result = new RedirectToRouteResult(
+                    new System.Web.Routing.RouteValueDictionary
+                    {
+                        { "controller", "Error" },
+                        { "action", "Unauthorized" }
+                    });
+            }
+            else
+            {
+                filterContext.Result = new HttpUnauthorizedResult();
+            }
+        }
+    }
+}
---
+++ RiseCup.Web/Controllers/ProductsController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using RiseCup.Database;
+using RiseCup.Domain.Models;
+using System.Data.Entity;
+using RiseCup.Web.Attributes;
+
+namespace RiseCup.Web.Controllers
+{
+    [SimpleAuthorize("Moderator", "Administrator")]
+    public class ProductsController : Controller
+    {
+        private RiseCupContext db = new RiseCupContext();
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            var products = db.Products.Include(p => p.Category).ToList();
+            return View(products);
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Name");
+            return View();
+        }
+
+        // POST: Products/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProductId,Name,Price,Description,CategoryId")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                product.ProductId = Guid.NewGuid().ToString();
+                db.Products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Name", product.CategoryId);
+            return View(product);
+        }
+
+// GET: Products/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Name", product.CategoryId);
+            return View(product);
+        }
+
+// POST: Products/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductId,Name,Price,Description,CategoryId")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Name", product.CategoryId);
+            return View(product);
+        }
+
+
+       
+
+        // GET: Products/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Product product = db.Products.Find(id);
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
